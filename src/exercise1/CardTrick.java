a26d61b7b81f3b0230b3d90ed0a73f6768e8fa6b
pate3193 <122803715+pate3193@users.noreply.github.com>
2023-01-26 18:37:30 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -8,8 +8,7 @@ import java.util.Scanner;
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Henil Patel Jan 25, 2023
  */
 public class CardTrick {
     
@@ -73,14 +72,14 @@ public class CardTrick {
     /**
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * @author Henil Patel Jan 2023
      */
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Henil Patel");
         System.out.println();
         
         System.out.println("My career ambitions:");

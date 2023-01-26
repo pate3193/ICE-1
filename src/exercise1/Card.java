721819f404762a5a9b10ffe3c08bb79bd0736bcd
pate3193 <pate3193@sheridancollege.ca>
2023-01-26 18:26:29 -0500

    student project version 2

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -43,4 +43,8 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }  
+    //  @Override
+    public String toString() {
+        return "Card{" + "suit='" + suit + '\'' + ", value=" + value +'}';}
+
 }
